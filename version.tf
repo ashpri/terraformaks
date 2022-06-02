e1465342b007c075ef1d3d5a476793b004645431
ashpri <86353178+ashpri@users.noreply.github.com>
2022-06-02 14:49:01 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -8,7 +8,12 @@ terraform {
 
   required_version = ">= 0.12"
 }
-# This block goes outside of the required_providers block!
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = var.client_secret
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
 }
